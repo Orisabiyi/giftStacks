70b7daae8231a92e7a02910fe8cf56d00253a040
Orisabiyi <orisabiyidavid@gmail.com>
2024-10-30 15:33:41 +0100

    refactor: Update Homepage.jsx to add styling classes to the navigation bar

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,16 +3,16 @@ import React from "react";
 export default function Homepage() {
   return (
     <>
-      <nav>
+      <nav className="flex items-center px-20 py-10">
         <h1 className="text-lg">GiftStacks</h1>
 
-        <ul>
+        <ul className="flex items-center gap-8 mx-auto">
           <li>Home</li>
           <li>Gift</li>
           <li>My Gifts</li>
         </ul>
 
-        <button>Connect Wallet</button>
+        <button className="bg-">Connect Wallet</button>
       </nav>
     </>
   );
