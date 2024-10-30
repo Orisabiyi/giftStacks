e34b9389c44fbadf4ed4a1f9ac5d91ac9c39c813
Orisabiyi <orisabiyidavid@gmail.com>
2024-10-30 13:41:04 +0100

    feat: bootstrap routes in the App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import { Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+
+export default function App() {
+  <BrowserRouter>
+    <Routes></Routes>
+  </BrowserRouter>;
+}
