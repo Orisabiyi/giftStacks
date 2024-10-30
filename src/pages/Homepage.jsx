611622eb52d7b82129917a924c82b4907cddde8f
Orisabiyi <orisabiyidavid@gmail.com>
2024-10-30 14:29:42 +0100

    chore: initialize the Homepage and navbar

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,17 @@
+export default function Homepage() {
+  return (
+    <>
+      <nav>
+        <h1>GiftStacks</h1>
+
+        <ul>
+          <li>Home</li>
+          <li>Gift</li>
+          <li>My Gifts</li>
+        </ul>
+
+        <button>Connect Wallet</button>
+      </nav>
+    </>
+  );
+}
