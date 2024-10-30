83798a7fd3f1afcecadcec6520bcf4cbc509503a
Orisabiyi <orisabiyidavid@gmail.com>
2024-10-30 16:27:26 +0100

    style: update the hover effect of the navlink

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,15 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Homepage() {
   return (
     <>
       <nav className="flex items-center px-20 py-10 text-16">
-        <h1 className="text-lg">GiftStacks</h1>
+        <h1 className="text-22 font-medium">/GiftStacks/</h1>
 
-        <ul className="flex items-center gap-8 mx-auto">
-          <li>Home</li>
-          <li>Gift</li>
-          <li>My Gifts</li>
+        <ul className="flex items-center gap-16 mx-auto">
+          <li className="hover:bg-primary hover:px-4 transition-all duration-700">
+            <Link>Home</Link>
+          </li>
+          <li className="hover:bg-primary hover:px-4 transition-all duration-700">
+            <Link>Gift</Link>
+          </li>
+          <li className="hover:bg-primary hover:px-4 transition-all duration-700">
+            <Link>My Gifts</Link>
+          </li>
         </ul>
 
         <button className="bg-primary px-8 py-4 rounded-xl">
