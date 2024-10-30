4e6f67d25766702816c16df8eaeec47e5d75444c
Orisabiyi <orisabiyidavid@gmail.com>
2024-10-30 18:08:02 +0100

    feat: add Landing page as nested route

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,15 @@ import { Routes } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
 
 import Homepage from "./pages/Homepage";
+import LandingPage from "./pages/LandingPage";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />}></Route>
+        <Route path="/" element={<Homepage />}>
+          <Route index element={<LandingPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
