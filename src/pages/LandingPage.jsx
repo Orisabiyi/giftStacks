40ec8af0e6db4d7430ec7328214cbe4cf25284cc
Orisabiyi <orisabiyidavid@gmail.com>
2024-10-31 20:43:14 +0100

    feat: update header text on LandingPage to emphasize gifting

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -6,7 +6,7 @@ export default function LandingPage() {
     <>
       <header className="h-screen flex flex-col items-center justify-center gap-16 animated-gradient">
         <h1 className="text-4 text-center uppercase font-semibold w-6/12">
-          Effortlessly send tokens and NFTs to friends and family
+          Effortlessly Gift tokens and NFTs to friends and family
         </h1>
         <button className="text-18 bg-white px-20 py-5 rounded-lg hover:bg-blue-700 hover:-translate-y-3 hover:text-white hover:font-medium transition-all duration-700">
           Start Gifting
