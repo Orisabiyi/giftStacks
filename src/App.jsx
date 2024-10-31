87625f98482050c84a8127e1037bb5e2cbbe1792
Orisabiyi <orisabiyidavid@gmail.com>
2024-10-31 02:37:38 +0100

    feat: creat gift stacks route

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Homepage />}>
           <Route index element={<LandingPage />} />
+          <Route path="gift-stacks" element={<LandingPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
