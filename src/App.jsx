aa3b57508945174f04cade7c74bf699ef2a4a192
Orisabiyi <orisabiyidavid@gmail.com>
2024-10-31 17:04:58 +0100

    feat: add prop-types dependency and wrap App component with AuthContextProvider

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,19 @@ import { BrowserRouter } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import LandingPage from "./pages/LandingPage";
 import GiftPage from "./pages/GiftPage";
+import { AuthContextProvider } from "./context/AuthContext";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Homepage />}>
-          <Route index element={<LandingPage />} />
-          <Route path="gift-stacks" element={<GiftPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <AuthContextProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />}>
+            <Route index element={<LandingPage />} />
+            <Route path="gift-stacks" element={<GiftPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AuthContextProvider>
   );
 }
