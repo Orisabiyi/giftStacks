9fdd7bf3c251206b1dd80b5cf40a24961fc5d800
Orisabiyi <orisabiyidavid@gmail.com>
2024-10-31 17:09:34 +0100

    style: update navigation and button styles to use blue color scheme

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -13,29 +13,29 @@ export default function Homepage() {
 
         <ul className="flex items-center gap-16 mx-auto">
           <li
-            className={`hover:bg-primary hover:px-4 transition-all duration-700 ${
-              pathname === "/" ? "bg-primary px-4" : ""
+            className={`hover:bg-blue-700 hover:px-4 transition-all duration-700 ${
+              pathname === "/" ? "bg-blue-700 px-4" : ""
             }`}
           >
             <Link to="/">Home</Link>
           </li>
           <li
-            className={`hover:bg-primary hover:px-4 transition-all duration-700 ${
-              pathname.includes("gift-stacks") ? "bg-primary px-4" : ""
+            className={`hover:bg-blue-700 hover:px-4 transition-all duration-700 ${
+              pathname.includes("gift-stacks") ? "bg-blue-700 px-4" : ""
             }`}
           >
             <Link to="gift-stacks">Gift</Link>
           </li>
           <li
-            className={`hover:bg-primary hover:px-4 transition-all duration-700 ${
-              pathname.includes("my-gifts") ? "bg-primary px-4" : ""
+            className={`hover:bg-blue-700 hover:px-4 transition-all duration-700 ${
+              pathname.includes("my-gifts") ? "bg-blue-700 px-4" : ""
             }`}
           >
             <Link to="my-gifts">My Gifts</Link>
           </li>
         </ul>
 
-        <button className="bg-primary px-8 py-4 rounded-xl">
+        <button className="bg-blue-700 text-white font-medium px-8 py-4 rounded-xl">
           Connect Wallet
         </button>
       </nav>
